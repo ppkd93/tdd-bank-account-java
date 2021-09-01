a0e31b09ea0194c40fe88405dd020747de50b1c2
Lisa <lisa.l.twomey@outlook.com>
2021-09-01 12:55:37 +0100

    null deposit initialised

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,6 +8,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
+    public void checkDepositIsNull() {
+        Account account = new Account();
+        assertThat(account.balance).isEqualTo(0);
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
         Account account = new Account();
         account.deposit(10);
